87f2471c135e802cc95843cea9785419bc2904d9
Chandler <90872682+ajchandler88@users.noreply.github.com>
2022-06-04 23:38:06 -0400

    Initial

---
+++ script.js
@@ -0,0 +1,7 @@
+const navBtn = document.querySelector("#navBtn");
+const navBar = document.querySelector(".navBar");
+
+navBtn.addEventListener('click', function onClick() {
+    navBar.classList.toggle('hidden');
+    });
+
